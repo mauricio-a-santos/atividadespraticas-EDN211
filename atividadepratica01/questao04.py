c5db3b8085003f33a743dda6a5363408fc14090b
mauricio-a-santos <smuryll@gmail.com>
2026-01-08 20:46:35 -0300

    atividade01 - questao04

---
+++ atividadepratica01/questao04.py
@@ -0,0 +1,8 @@
+nome_do_produto = "Cadeira Infantil"
+Preco_unitario = 12.40
+quantidade = 3
+valor_final = Preco_unitario * quantidade
+print("Produto:", nome_do_produto)
+print(f"Preco unitario: {Preco_unitario:.2f}")
+print("Quantidade:", quantidade)
+print(f"o valor final da compra é: R${valor_final:.2f}")
