f6cb24b3bff70568fb3cbfe132978fba9c47af87
mauricio-a-santos <smuryll@gmail.com>
2026-01-09 20:35:20 -0300

    Atividade pratica02 - questao03

---
+++ atividadepratica02/questao03.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+nota3 = float(input("Digite a nota 3: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"nota 1: {nota1:.2f}")
+print(f"nota 2: {nota2:.2f}")
+print(f"nota 3: {nota3:.2f}")
+print(f"média: {media:.2f}")
