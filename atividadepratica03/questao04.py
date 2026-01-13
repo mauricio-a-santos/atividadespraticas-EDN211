ff3275babe5527c005cccefdd91f0a46560d3486
mauricio-a-santos <smuryll@gmail.com>
2026-01-12 22:03:07 -0300

    mudança do nome da questão03 da atividade 03 e envio da questão04 da atividade 03

---
+++ atividadepratica03/questao04.py
@@ -0,0 +1,12 @@
+ano = int(input("Digite o ano: "))
+
+if ano % 4 == 0:
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print("O ano é bissexto")
+        else:
+            print("O ano não é bissexto")
+    else:
+        print("O ano é bissexto")
+else:
+    print("O ano não é bissexto")
