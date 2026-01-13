f1105fe8df782eb1b49117d34e01f512eeb52e1b
mauricio-a-santos <smuryll@gmail.com>
2026-01-12 21:46:10 -0300

    atividadepratica03 - questao03

---
+++ atividadepratica03/quastao03.py
@@ -0,0 +1,25 @@
+temperatura = float(input("Digite a temperatura: "))
+origem = input("Digite a origem da temperatura (C, F ou K)").upper()
+destino = input("Digite a origem da temperatura (C, F ou K)").upper()
+
+if origem == destino:
+    resultado = temperatura
+elif origem == "C":
+    if destino == "F":
+        resultado = (temperatura * 9/5) + 32
+    else:
+        resultado = temperatura + 273.15
+
+elif origem == "F":
+    if destino == "C":
+        resultado = (temperatura - 32) * 5/9
+    else:
+        resultado = (temperatura - 32) * 5/9 + 273.15
+
+else:
+    if destino == "C":
+        resultado = temperatura - 273.15
+    else:
+        resultado = (temperatura - 273.15) * 5/9 + 273.15
+
+print(f"{temperatura} {origem} é igual a {resultado} {destino}")
